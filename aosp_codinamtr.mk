85cb006ff714c78f596d7661086b94edc005d8be
C457 <android.c357@gmail.com>
2013-10-31 23:50:11 -0500

    Rename AOSP builds to aosp_*

--- aosp_codinamtr.mk
+++ aosp_codinamtr.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2013 The Android Open Source Project
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+$(call inherit-product, device/samsung/codinamtr/full_codinamtr.mk)
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_codinamtr.mk \
-    $(LOCAL_DIR)/full_codinamtr.mk
+PRODUCT_NAME := aosp_codinamtr
+
+PRODUCT_PACKAGES += \
+    Launcher3
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -34,4 +34,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cm_codinamtr-userdebug
+add_lunch_combo aosp_codinamtr-userdebug
